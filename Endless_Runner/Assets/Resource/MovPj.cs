814f1470d263d1177893297eb109fba13f007ecd
jsmzdf <sebastianjhon9427@gmail.com>
2020-05-11 20:20:05 -0500

    se empieza la primera mecánica

---
+++ Endless_Runner/Assets/Resource/MovPj.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovPj : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = transform.position.z;
+        if(Input.GetKey(KeyCode.A) ){
+            
+            transform.Translate(0,0,Time.deltaTime+1);
+            
+        }
+         if(Input.GetKey(KeyCode.D) ){
+            
+            transform.Translate(0,0,Time.deltaTime-1);
+
+        }
+             Debug.Log(x);
+        
+        
+    }
+}
